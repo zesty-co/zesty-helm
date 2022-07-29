2c4f7faeccf7d7051cd7b7863e8c31cca5665d59
Omer Hamerman <omerxx@gmail.com>
2022-07-29 18:54:17 +0100

    remove bump script

--- scripts/vbump.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-current_version=$(yq .version Chart.yaml)
-bumped_version=$(echo "${current_version}" | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
-BUMPED=${bumped_version} yq -i '.version = strenv(BUMPED)' Chart.yaml
