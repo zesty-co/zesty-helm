f7c3675d7880fd64b7faefda0bc7667f3c102d5d
Omer Hamerman <omerxx@gmail.com>
2022-07-29 15:07:52 +0100

    release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,3 +1,25 @@
-scripts/vbump.sh
-helm package .
-helm repo index .
+#!/bin/bash
+
+bump_version() {
+  current_version=$(yq .version Chart.yaml)
+  bumped_version=$(echo "${current_version}" | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
+  BUMPED=${bumped_version} yq -i '.version = strenv(BUMPED)' Chart.yaml
+  RELEASE_FILE="zesty-${bumped_version}.tgz"
+}
+
+pack() {
+  helm package .
+}
+
+index() {
+  helm repo index "https://github.com/zesty-co/zesty-helm/releases/download/${bumped_version}/${RELEASE_FILE}" .
+}
+
+release() {
+  gh release create "${bumped_version}" "${RELEASE_FILE}" --generate-notes --prerelease --title "New ZD release"
+}
+
+bump_version
+pack
+index
+release
