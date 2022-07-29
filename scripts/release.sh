5cf6b4efc89e8e6cbeca9e929b25bdd059b79edf
Omer Hamerman <omerxx@gmail.com>
2022-07-29 18:51:12 +0100

    release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -12,13 +12,7 @@ pack() {
 }
 
 index() {
-  helm repo index --url "https://github.com/zesty-co/zesty-helm/releases/download/${bumped_version}/${RELEASE_FILE}" .
-}
-
-git_actions() {
-  git add "*"
-  git commit "auto-release"
-  git push origin upstream
+  helm repo index --url "https://github.com/zesty-co/zesty-helm/releases/download/${bumped_version}" .
 }
 
 release() {
@@ -28,5 +22,4 @@ release() {
 bump_version
 pack
 index
-git_actions
 release
